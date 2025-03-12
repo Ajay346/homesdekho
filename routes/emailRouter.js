1db9dd38f32c7564291e0c8ae2dbe0145423b446
Ajay Shinde <84243765+Ajay346@users.noreply.github.com>
2025-03-12 16:50:17 +0530

    main

--- routes/emailRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { emailGenerator } from "../controller/emailController.js";
-
-const router = express.Router();
-
-router.post("/send-email", emailGenerator);
-
-export default router;
